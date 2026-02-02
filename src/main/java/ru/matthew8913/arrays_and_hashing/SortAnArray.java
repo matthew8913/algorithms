b27879a387a0700dee0c72a89413198197673397
matthew <matvey.yurkevich@gmail.com>
2026-02-02 20:59:49 +0700

    solved: leetcode №912, 75

---
+++ src/main/java/ru/matthew8913/arrays_and_hashing/SortAnArray.java
@@ -0,0 +1,49 @@
+package ru.matthew8913.arrays_and_hashing;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+class SortAnArray {
+    public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+
+    public void sort(int[] nums, int lStart, int rStart) {
+        if(lStart>=rStart) return;
+
+        int pivot = nums[lStart];
+
+        int l = lStart, r = rStart;
+
+        while(l<=r){
+            while(nums[l]<pivot) l++;
+            while(nums[r]>pivot) r--;
+
+            if (l<=r){
+                swap(nums, l, r);
+                l++;
+                r--;
+            }
+        }
+
+        sort(nums, lStart, r);
+        sort(nums, l, rStart);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 6, 3, 1, 7, 2, 3};
+        SortAnArray sortAnArray = new SortAnArray();
+        sortAnArray.sortArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
---
+++ src/main/java/ru/matthew8913/arrays_and_hashing/SortColors.java
@@ -0,0 +1,48 @@
+package ru.matthew8913.arrays_and_hashing;
+
+import java.util.Arrays;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int oneInd = 0, zeroInd = 0, twoInd = 0;
+        for (int i = 0; i<nums.length; i++){
+            switch (nums[i]){
+                case 0:
+                    nums[twoInd++] = 2;
+                    nums[oneInd++] = 1;
+                    nums[zeroInd++] = 0;
+                    break;
+                case 1:
+                    nums[twoInd++] = 2;
+                    nums[oneInd++] = 1;
+                    break;
+                case 2:
+                    nums[twoInd++] = 2;
+                    break;
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        SortColors sortColors = new SortColors();
+        int[] arr1 = new int[]{2,2,2,1,0,2,1,0,1};
+        sortColors.sortColors(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        int[] arr2 = new int[]{0,0,0};
+        sortColors.sortColors(arr2);
+        System.out.println(Arrays.toString(arr2));
+
+        int[] arr3 = new int[]{2,2,2};
+        sortColors.sortColors(arr3);
+        System.out.println(Arrays.toString(arr3));
+
+        int[] arr4 = new int[]{1,1,1};
+        sortColors.sortColors(arr4);
+        System.out.println(Arrays.toString(arr4));
+
+
+    }
+}
